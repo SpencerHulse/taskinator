f3f79ce368be0e9d89f3c63f0b53201bb38a78e4
Spencer Hulse <hulse.spencer@gmail.com>
2021-12-26 21:35:20 -0500

    added form field value capture functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,29 @@ let tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 let createTaskHandler = (event) => {
   event.preventDefault();
+  let taskNameInput = document.querySelector("input[name='task-name']").value;
+  let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  //create list item
   let listItemE1 = document.createElement("li");
-
   listItemE1.className = "task-item";
-  listItemE1.textContent = "This is a new task.";
+
+  //create div to hold task info and add to list item
+  let taskInfoE1 = document.createElement("div");
+  //give it a class name
+  taskInfoE1.className = "task-info";
+
+  //add HTML to div
+  taskInfoE1.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+
+  listItemE1.appendChild(taskInfoE1);
+
+  //add entire list item to list
   tasksToDoE1.appendChild(listItemE1);
 };
 
